177aa2379b32335203ec346c3ded8ed44e1767c8
Carlos Arturo <carlox216@utp.edu.co>
2018-11-05 17:58:17 -0500

    setup static files and first tempaltes
    set on settings.py static root  directory and set templates root directory

--- mercaTODO/sites/views.py
+++ mercaTODO/sites/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.template import loader
 
-# Create your views here.
+
+def Index(request):
+	ctx={}
+	template = loader.get_template('sites/index.html')
+
+	return HttpResponse(template.render(ctx, request))
+	
+def Logout(request):
+	if request.user is not None:
+
+		logout(request)
+	return redirect('index')
